c3efde22a0eec8eeaef650206cf44b723e6ac2ce
giovannagrigo <giovanna1grigo@gmail.com>
2025-06-02 20:26:33 -0300

    Finalização da avaliação

---
+++ 02_PedraPapelTesoura/PedraPapelTesoura/Game.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PedraPapelTesoura
+{
+    public class Game
+    {
+        public string jogar(string escolhaA, string escolhaB)
+        {
+            if (escolhaA == escolhaB)
+                return "Empate";
+
+            if (escolhaA == "Pedra" && escolhaB == "Papel")
+                return "Pedra vence Papel!";
+
+            if (escolhaA == "Pedra" && escolhaB == "Tesoura")
+                return "Pedra vence Tesoura!";
+
+            if (escolhaA == "Papel" && escolhaB == "Pedra")
+                return "Pedra vence Papel!";
+
+            if (escolhaA == "Papel" && escolhaB == "Tesoura")
+                return "Tesoura vence Papel!";
+
+            return "Tesoura vence" + escolhaB + "!";
+        }
+    }
+}
---
+++ 02_PedraPapelTesoura/PedraPapelTesouraTest/GameUnitTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Drawing;
+using PedraPapelTesoura;
+
+namespace PedraPapelTesouraTest
+{
+    [TestClass]
+    public sealed class GameUnitTest
+    {
+        private Game game;
+
+        [TestInitialize]
+        public void setup()
+        {
+            game = new Game();
+        }
+
+        [TestMethod]
+        [DataRow("Pedra", "Papel", "Papel vence Pedra!")]
+        [DataRow("Pedra", "Tesoura", "Pedra vence Tesoura!")]
+        [DataRow("Pedra", "Pedra", "Empate")]
+        [DataRow("Papel", "Pedra", "Papel vence Pedra!")]
+        [DataRow("Papel", "Tesoura", "Tesoura vence Papel!")]
+        [DataRow("Papel", "Papel", "Empate")]
+        [DataRow("Tesoura", "Papel", "Tesoura vence Papel!")]
+        [DataRow("Tesoura", "Pedra", "Pedra vence Tesoura!")]
+        [DataRow("Tesoura", "Tesoura", "Empate")]
+        public void DeveRetornarResultadoJogo(string escolhaA, string escolhaB, string esperado)
+        {
+            // Act
+            string obtido = game.jogar(escolhaA, escolhaB);
+
+            // Assert  
+            Assert.AreEqual(esperado, obtido);
+        }
+    }
+}
