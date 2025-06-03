70d4b2f3c73104f9905af29622b17541c89de5c3
giovannagrigo <giovanna1grigo@gmail.com>
2025-06-02 21:05:16 -0300

    Comentários

--- 02_PedraPapelTesoura/PedraPapelTesoura/Game.cs
+++ 02_PedraPapelTesoura/PedraPapelTesoura/Game.cs
@@ -14,13 +14,13 @@ namespace PedraPapelTesoura
                 return "Empate";
 
             if (escolhaA == "Pedra" && escolhaB == "Papel")
-                return "Pedra vence Papel!";
+                return "Pedra vence Papel!"; // Incorreto, Pedra deveria perder para o papel
 
             if (escolhaA == "Pedra" && escolhaB == "Tesoura")
                 return "Pedra vence Tesoura!";
 
             if (escolhaA == "Papel" && escolhaB == "Pedra")
-                return "Pedra vence Papel!";
+                return "Pedra vence Papel!"; // Incorreto novamente, Pedra deveria perder para o papel
 
             if (escolhaA == "Papel" && escolhaB == "Tesoura")
                 return "Tesoura vence Papel!";
